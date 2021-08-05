ccc2bcc94e3fc9e0a71af385a29de485010ffd34
Pierre Dagorn <pierre.dagorn@bassetti.fr>
2021-08-05 16:12:03 +0200

    indentation

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ client = discord.Client()
 
 
 def get_quote():
-      response = requests.get("https://zenquotes.io/api/random")
-  json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return(quote)
 
 @client.event
 async def on_ready():
